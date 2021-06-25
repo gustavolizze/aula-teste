991cae424da5590c2b5b8e7fe81d8fd85b5d0441
gustavolizze <gustavolizze@hotmail.com>
2021-06-25 15:22:12 -0300

    Fix: file send

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-
-app.use(express.static(__dirname + '/views'));
 
 app.get('/ping', function (req, res) {
   res.send('pong');
 });
 
 app.get('/', function (req, res) {
-  res.render('login.html');
+  res.sendFile(path.join(__dirname + '/views/login.html'));
 });
 
 app.post('/', function (req, res) {});
