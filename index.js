b0e2d5fa70e4d9c89029717b1637706e02a7f567
gustavolizze <gustavolizze@hotmail.com>
2021-06-25 16:01:30 -0300

    chore: added cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.get('/ping', function (req, res) {
   res.send('pong');
